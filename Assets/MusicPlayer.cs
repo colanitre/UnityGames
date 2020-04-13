04fd9fc2eaa4bf1c41a0856b31fa69023cb4b7fd
colanitre <colanitre@gmail.com>
2020-04-13 12:13:56 +0200

    Added music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float levelLoadDelay = 5f;
+
+    void Start()
+    {
+        Invoke("LoadFirstScene", levelLoadDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
